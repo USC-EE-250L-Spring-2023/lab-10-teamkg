2090127436ed95a2f068e2b89f0463dd948fd5c1
Karla Vasquez <89766689+tinkerpinky10@users.noreply.github.com>
2023-04-15 00:09:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def generate_data() -> List[int]:
     return np.random.randint(100, 10000, 1000).tolist()
 
 def process1(data: List[int]) -> List[int]:
-    """TODO: Document this function. What does it do? What are the inputs and outputs?"""
+    """TODO: Document this function. What does it do? What are the inputs and outputs?
+    process1 passes in a list data structure that consists of ints and returns this data structure with ints"""
+    
     def foo(x):
         """Find the next largest prime number."""
         while True:
@@ -22,7 +24,8 @@ def process1(data: List[int]) -> List[int]:
     return [foo(x) for x in data]
 
 def process2(data: List[int]) -> List[int]:
-    """TODO: Document this function. What does it do? What are the inputs and outputs?"""
+    """TODO: Document this function. What does it do? What are the inputs and outputs?
+    process2 takes in a list data structure as data input and returns a list of the integers """
     def foo(x):
         """Find the next largest prime number."""
         while True:
@@ -32,10 +35,11 @@ def process2(data: List[int]) -> List[int]:
     return [foo(x) for x in data]
 
 def final_process(data1: List[int], data2: List[int]) -> List[int]:
-    """TODO: Document this function. What does it do? What are the inputs and outputs?"""
+    """TODO: Document this function. What does it do? What are the inputs and outputs?
+    final_process takes in two list data structures consisting of integers and returns the average"""
     return np.mean([x - y for x, y in zip(data1, data2)])
 
-offload_url = 'http://192.168.4.74:5000' # TODO: Change this to the IP address of your server
+offload_url = 'http://192.168.2.74:5000' # TODO: Change this to the IP address of your server
 
 def run(offload: Optional[str] = None) -> float:
     """Run the program, offloading the specified function(s) to the server.
@@ -55,7 +59,8 @@ def run(offload: Optional[str] = None) -> float:
         def offload_process1(data):
             nonlocal data1
             # TODO: Send a POST request to the server with the input data
-            data1 = response.json()
+            theResponse = requests.post(f'{offload_url}/process1', json=data)
+            data1 = theResponse.json() 
         thread = threading.Thread(target=offload_process1, args=(data,))
         thread.start()
         data2 = process2(data)
@@ -68,9 +73,35 @@ def run(offload: Optional[str] = None) -> float:
     elif offload == 'process2':
         # TODO: Implement this case
         pass
+        data2 = None
+        def offload_process2(data):
+            nonlocal data2
+            theResponse = requests.post(f'{offload_url}/process2', json=data)
+            data2 = theResponse.json() 
+        thread = threading.Thread(target=offload_process2, args=(data,))
+        thread.start()
+        data1 = process1(data)
+        thread.join()
+
     elif offload == 'both':
         # TODO: Implement this case
         pass
+        data1 = None
+        data2 = None
+        def offload_process1(data):
+            nonlocal data1
+            theResponse = requests.post(f'{offload_url}/process1', json=data)
+            data1 = theResponse.json()
+        def offload_process2(data):
+            nonlocal data2
+            theResponse = requests.post(f'{offload_url}/process2', json=data)
+            data2 = theResponse.json() 
+        thread1 = threading.Thread(target=offload_process1, args=(data,))
+        thread1.start()
+        thread1.join()
+        thread2 = threading.Thread(target=offload_process2, args=(data,))
+        thread2.start()
+        thread2.join()
 
     ans = final_process(data1, data2)
     return ans 
@@ -79,14 +110,33 @@ def main():
     # TODO: Run the program 5 times for each offloading mode, and record the total execution time
     #   Compute the mean and standard deviation of the execution times
     #   Hint: store the results in a pandas DataFrame, use previous labs as a reference
-
+    rows =[] 
+    totalTime = 0
+    samples = 5
+    listofModes = {"None", "process1", "process2", "both"}
+    for i in listofModes:
+        times = []
+        for j in range(samples):
+            start = time.time()
+            run(i)
+            end = time.time() #end - start 
+            totalTime = end - start 
+            times.append(totalTime)
+            print(f"Offloading {i} - sample{j+1}: {times[-1]:.2f}")
+        rows.append([str(i), np.mean(times), np.std(times)])
+    
+    df = pd.DataFrame(rows, columns=['Mode', 'Mean Time', 'Standard Deviation Time'])
 
     # TODO: Plot makespans (total execution time) as a bar chart with error bars
     # Make sure to include a title and x and y labels
-
+    graph = px.bar(df,
+            x='Mode',
+            y='Mean Time',
+            error_y='Standard Deviation Time'
+    )
 
     # TODO: save plot to "makespan.png"
-
+    graph.write_image('makespan.png')
 
     # Question 4: What is the best offloading mode? Why do you think that is?
     # Question 5: What is the worst offloading mode? Why do you think that is?
